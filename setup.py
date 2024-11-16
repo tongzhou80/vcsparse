87a8a05493676cf1fe22f2692c8656c05492c4eb
Tong Zhou <zt9465@gmail.com>
2024-11-16 13:16:23 +0800

    updated to use ast_transforms

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.9',            # Minimum Python version requirement
-    install_requires=[],
+    install_requires=['astlib'],
 )
