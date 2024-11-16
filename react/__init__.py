dc2c18dc975be604dbf8be05f36fd386f449bd80
Tong Zhou <zt9465@gmail.com>
2024-11-16 11:21:09 +0800

    added functionality to print the indices

--- react/__init__.py
+++ react/__init__.py
@@ -7,8 +7,8 @@ def Index(*args):
 def compile_from_src(src, **options):
     tree = ast.parse(src)
     tree = to_single_op_form.transform(tree)
-    #tree = attach_index_notation.transform(tree)
+    tree = attach_index_notation.transform(tree)
     return ast_to_code(tree)
 
 def ast_to_code(tree):
-    return ast.unparse(tree).replace('# type:', '# ')
+    return ast.unparse(tree).replace('# type:', '#')
--- react/transforms/attach_index_notation.py
+++ react/transforms/attach_index_notation.py
@@ -20,8 +20,11 @@ class AttachIndexNotation(ast.NodeTransformer):
 
     def visit_BinOp(self, node):
         if isinstance(node.op, (ast.Add, ast.Sub, ast.Mult, ast.Div)):
-            assert isinstance(node.left, ast.Name)
-            node.indices = self.indices_map[node.left.id]
+            node.indices = []
+            for operand in [node.left, node.right]:
+                if isinstance(operand, ast.Name):
+                    if len(self.indices_map[operand.id]) > len(node.indices):
+                        node.indices = self.indices_map[operand.id]
         elif isinstance(node.op, ast.MatMult):
             assert isinstance(node.left, ast.Name) and isinstance(node.right, ast.Name)
             a, b = node.left.id, node.right.id
@@ -41,6 +44,7 @@ class AttachIndexNotation(ast.NodeTransformer):
         assert isinstance(node.targets[0], ast.Name)
         self.generic_visit(node)
         self.indices_map[node.targets[0].id] = node.value.indices
+        node.type_comment = 'indices: ' + str(node.value.indices)
         return node
 
 def transform(tree):
