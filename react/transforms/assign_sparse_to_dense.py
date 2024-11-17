43dba82f164fdb75da4855139d8c986f152710b1
Tong Zhou <zt9465@gmail.com>
2024-11-17 21:36:48 +0800

    added pass assign_sparse_to_dense, which seems working

---
+++ react/transforms/assign_sparse_to_dense.py
@@ -0,0 +1,57 @@
+import ast
+from ast_transforms.utils import *
+
+class AssignSparseToDense(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        '''
+        Record the storage format of the arguments based on annotations
+        '''
+        self.tensor_format = {}
+        for arg in node.args.args:
+            varname = arg.arg
+            if hasattr(arg, 'annotation') and arg.annotation is not None:
+                # The default format is dense
+                self.tensor_format[varname] = 'dense'
+                if len(arg.annotation.args) > 1:
+                    self.tensor_format[varname] = arg.annotation.args[1].value
+        self.generic_visit(node)
+        return node
+
+    def visit_Assign(self, node):
+        '''
+        Check if the assignment uses any operand that is sparse, and seperate it out 
+        to a new assignment if so.
+        '''
+        visitor = ReplaceSparseOperands(self.tensor_format)
+        visitor.visit(node)
+        return visitor.new_stmts + [node]
+        
+class ReplaceSparseOperands(ast.NodeTransformer):
+    def __init__(self, tensor_format):
+        self.tensor_format = tensor_format
+        self.new_stmts = []
+        self.var_count = 0
+
+    def get_new_var(self):
+        self.var_count += 1
+        return '__dt%d' % self.var_count  # stands for "dense temporary"
+
+    def visit_Name(self, node):
+        if node.id in self.tensor_format:
+            if self.tensor_format[node.id] in ('csr', 'csc'):
+                # We need to make a dense temporary
+                # and assign the sparse operand to it
+                # and then replace the original operand with the temporary
+                tmp = self.get_new_var()
+                self.new_stmts.append(
+                    new_ast_assign(
+                        new_ast_name(tmp),
+                        node
+                    )
+                )
+                return new_ast_name(tmp)
+
+        return node
+
+def transform(node):
+    return AssignSparseToDense().visit(node)
