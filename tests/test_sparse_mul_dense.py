184f540421f1e9d086107063c549b4351597408a
Tong Zhou <zt9465@gmail.com>
2024-11-22 17:24:20 +0800

    added test_sparse_mul_dense.py

---
+++ tests/test_sparse_mul_dense.py
@@ -0,0 +1,24 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+def f0(A: Tensor('i,j', 'csr'), B: Tensor('i,j')):
+    return A * B
+
+def f1(A: Tensor('i,j', 'csr'), alpha):
+    return A * alpha
+
+f0 = compile(f0, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True, memory_opt=True)
+f1 = compile(f1, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True, memory_opt=True)
+
+for N in [1000, 4000]:
+    A = sp.random(N, N, density=0.01, format='csr')
+    B = np.random.randn(N, N)
+    alpha = 0.1
+    #print(type(A.multiply(B)), type(A * alpha))
+    #print(np.allclose(A.multiply(B).toarray(), A * B))
+    assert np.allclose(f0(A, B), (A.multiply(B)).toarray())
+    assert np.allclose(f1(A, alpha), (A * alpha).toarray())
+    
+    
