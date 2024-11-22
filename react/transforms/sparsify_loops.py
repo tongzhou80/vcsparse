a1af66128fad106b3bb0aafdc0030d615199b246
Tong Zhou <zt9465@gmail.com>
2024-11-22 17:43:21 +0800

    added ConvertDenseLoopToSparseNew; to test

--- react/transforms/sparsify_loops.py
+++ react/transforms/sparsify_loops.py
@@ -28,6 +28,35 @@ class ConvertDenseLoopToSparse(ast.NodeTransformer):
         
         return node
 
+class ConvertDenseLoopToSparseNew(ast.NodeTransformer):
+    def __init__(self, var, format, indices_map):
+        self.var = var
+        self.format = format
+        self.indices_map = indices_map
+        if self.format == 'csr':
+            self.sparse_index = self.indices_map[self.var][1]
+        else:
+            assert False, "Non-CSR format not supported yet"
+
+    def visit_For(self, node):
+        self.generic_visit(node)
+        if node.target.id == self.sparse_index:
+            outer_index = self.indices_map[self.var][0]
+            node.iter = new_ast_range(
+                stop=new_ast_node_from_str(f'{self.var}.indptr[{outer_index}+1]'),
+                start=new_ast_node_from_str(f'{self.var}.indptr[{outer_index}]'),
+            )
+            new_inner_index = f'__p{self.var}_{outer_index}'
+            node.target = new_ast_name(new_inner_index)
+            node.body.insert(0, new_ast_assign(
+                new_ast_name(inner_index, ctx=ast.Store()),
+                new_ast_node_from_str(f'{self.var}.indices[{new_inner_index}]')
+            ))
+            RewriteSparseTensorRead(self.var, new_inner_index).visit(node)
+
+        return node
+            
+
 class RewriteSparseTensorRead(ast.NodeTransformer):
     '''
     Rewrite A[i,k] to A.data[_k] if A is a sparse tensor.
