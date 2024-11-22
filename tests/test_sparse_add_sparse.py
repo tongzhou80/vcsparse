ffc3c66ba4ff6fd0fd03745b2a86240cc1ff4d50
Tong Zhou <zt9465@gmail.com>
2024-11-22 10:57:29 +0800

    added test test_sparse_add_sparse

---
+++ tests/test_sparse_add_sparse.py
@@ -0,0 +1,24 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+def f0(A: Tensor('i,j', 'csr'), B: Tensor('i,j', 'csr')):
+    # sparse add sparse
+    return A + B
+
+def f1(A: Tensor('i,j', 'csr'), B: Tensor('i,j', 'csr'), C: Tensor('i,j', 'csr')):
+    # add three sparse
+    return A + B + C
+
+f0 = compile(f0, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True)
+f1 = compile(f1, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True)
+
+for N in [1000, 4000]:
+    A = sp.random(N, N, density=0.01, format='csr')
+    B = sp.random(N, N, density=0.01, format='csr')
+    C = sp.random(N, N, density=0.01, format='csr')
+    assert np.allclose(f0(A, B), (A + B).toarray())
+    assert np.allclose(f1(A, B, C), (A + B + C).toarray())
+    
+    
