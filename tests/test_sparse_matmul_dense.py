4390f401afa7236c101840600ee0ce86720396e8
Tong Zhou <zt9465@gmail.com>
2024-11-23 11:31:19 +0800

    added test spmm

---
+++ tests/test_sparse_matmul_dense.py
@@ -0,0 +1,23 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+def f0(A: Tensor('i,k', 'csr'), B: Tensor('k,j')):
+    return A @ B
+
+f0 = compile(f0, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True, memory_opt=True)
+
+for N in [1000, 4000]:
+    A = sp.random(N, N, density=0.01, format='csr')
+    B = np.random.randn(N, N)
+    
+    #print(type(A.multiply(B)), type(A * alpha))
+    #print(np.allclose(A.multiply(B).toarray(), (A * B).toarray()))
+    assert np.allclose(f0(A, B), A @ B)
+    
+    t0 = timeit.timeit(lambda: (A @ B), number=10) / 10
+    t1 = timeit.timeit(lambda: f0(A, B), number=10) / 10
+    print(t0, t1, f'{(t0/t1):.3f}')
+    
+    
