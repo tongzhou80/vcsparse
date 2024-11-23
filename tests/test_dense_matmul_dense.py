85a33a2011d53abfe46c169cd27fc4676d24b046
Tong Zhou <zt9465@gmail.com>
2024-11-23 14:27:46 +0800

    add test

---
+++ tests/test_dense_matmul_dense.py
@@ -0,0 +1,22 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+@compile(dump_code=True, full_opt=True)
+def f0(A: Tensor('i,k'), B: Tensor('k,j')):
+    return A @ B
+
+def test():
+    for N in [1000]:
+        A = np.random.randn(N, N)
+        B = np.random.randn(N, N)
+        assert np.allclose(f0(A, B), A @ B)
+        
+        t0 = timeit.timeit(lambda: (A @ B), number=10) / 10
+        t1 = timeit.timeit(lambda: f0(A, B), number=10) / 10
+        print(t0, t1, f'{(t0/t1):.3f}')
+
+
+if __name__ == '__main__':
+    test()
