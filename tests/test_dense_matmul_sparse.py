013f49882329abc74c87252cddf3ddd661b4554e
Tong Zhou <zt9465@gmail.com>
2024-11-23 14:15:49 +0800

    refactored all tests to pytest form

--- tests/test_dense_matmul_sparse.py
+++ tests/test_dense_matmul_sparse.py
@@ -3,18 +3,20 @@ import numpy as np
 import timeit
 from react import *
 
+@compile(dump_code=True, full_opt=True)
 def f0(A: Tensor('i,k'), B: Tensor('k,j', 'csr')):
     return A @ B
 
-f0 = compile(f0, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True, memory_opt=True)
+def test_dense_matmul_sparse():
+    for N in [1000, 4000]:
+        A = np.random.randn(N, N)
+        B = sp.random(N, N, density=0.01, format='csr')
+        assert np.allclose(f0(A, B), A @ B)
+        
+        t0 = timeit.timeit(lambda: (A @ B), number=10) / 10
+        t1 = timeit.timeit(lambda: f0(A, B), number=10) / 10
+        print(t0, t1, f'{(t0/t1):.3f}')
 
-for N in [1000, 4000]:
-    A = np.random.randn(N, N)
-    B = sp.random(N, N, density=0.01, format='csr')
-    assert np.allclose(f0(A, B), A @ B)
-    
-    t0 = timeit.timeit(lambda: (A @ B), number=10) / 10
-    t1 = timeit.timeit(lambda: f0(A, B), number=10) / 10
-    print(t0, t1, f'{(t0/t1):.3f}')
-    
-    
+
+if __name__ == '__main__':
+    test_dense_matmul_sparse()
