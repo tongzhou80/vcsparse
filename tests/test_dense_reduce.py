3cc8dfc69dd030df74d173f5cd8d049f5653a61b
Tong Zhou <zt9465@gmail.com>
2024-11-23 19:08:38 +0800

    added reduction tests

---
+++ tests/test_dense_reduce.py
@@ -0,0 +1,26 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+@compile(dump_code=True, full_opt=True)
+def f0(A: Tensor('i,j')):
+    b = sum(A, 1)
+    return A / b[:, None]
+
+def f0_py(A):
+    b = np.sum(A, 1)
+    return A / b[:, None]
+
+def test():
+    for N in [1000, 4000]:
+        A = np.random.randn(N, N)
+        assert np.allclose(f0(A), f0_py(A))
+        
+        t0 = timeit.timeit(lambda: f0_py(A), number=10) / 10
+        t1 = timeit.timeit(lambda: f0(A), number=10) / 10
+        print(t0, t1, f'{(t0/t1):.3f}')
+
+    
+if __name__ == '__main__':
+    test()
---
+++ tests/test_sparse_reduce.py
@@ -0,0 +1,26 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+@compile(dump_code=True, full_opt=True)
+def f0(A: Tensor('i,j', 'csr')):
+    b = sum(A, 1)
+    return A / b[:, None]
+
+def f0_py(A):
+    b = np.sum(A, 1)
+    return A / b
+
+def test():
+    for N in [1000, 4000]:
+        A = sp.random(N, N, density=0.01, format='csr')
+        assert np.allclose(f0(A), f0_py(A).toarray())
+        
+        t0 = timeit.timeit(lambda: f0_py(A), number=10) / 10
+        t1 = timeit.timeit(lambda: f0(A), number=10) / 10
+        print(t0, t1, f'{(t0/t1):.3f}')
+
+    
+if __name__ == '__main__':
+    test()
