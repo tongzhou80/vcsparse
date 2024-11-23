44890dccf8883a9d9842fc8c7ad0e2d0f3c07c5f
Tong Zhou <zt9465@gmail.com>
2024-11-23 14:00:10 +0800

    converted test_sparse_mul_dense to pytest form

--- tests/test_sparse_mul_dense.py
+++ tests/test_sparse_mul_dense.py
@@ -3,20 +3,22 @@ import numpy as np
 import timeit
 from react import *
 
+@compile(dump_code=True, full_opt=True)
 def f0(A: Tensor('i,j', 'csr'), B: Tensor('i,j')):
     return A * B
 
+@compile(dump_code=True, full_opt=True)
 def f1(A: Tensor('i,j', 'csr'), alpha):
     return A * alpha
 
-f0 = compile(f0, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True, memory_opt=True)
-f1 = compile(f1, dump_code=True, trie_fuse=True, gen_numba_code=True, parallelize=True, memory_opt=True)
+def test_sparse_mul_dense():
+    for N in [1000, 4000]:
+        A = sp.random(N, N, density=0.01, format='csr')
+        B = np.random.randn(N, N)
+        alpha = 0.1    
+        assert np.allclose(f0(A, B), (A.multiply(B)).toarray())
+        assert np.allclose(f1(A, alpha), (A.multiply(alpha)).toarray())
 
-for N in [1000, 4000]:
-    A = sp.random(N, N, density=0.01, format='csr')
-    B = np.random.randn(N, N)
-    alpha = 0.1    
-    assert np.allclose(f0(A, B), (A.multiply(B)).toarray())
-    assert np.allclose(f1(A, alpha), (A.multiply(alpha)).toarray())
-    
-    
+
+if __name__ == '__main__':
+    test_sparse_mul_dense()
