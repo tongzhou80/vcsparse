bb535acfcaed705227eead9f33bdec1636627ebd
Tong Zhou <zt9465@gmail.com>
2024-11-23 14:27:34 +0800

    moved the check_for_undefined pass into react

--- react/__init__.py
+++ react/__init__.py
@@ -7,7 +7,7 @@ from ast_transforms import apply_transform_on_ast
 from .transforms import attach_index_notation, op_to_loop, trie_fuse, insert_allocations, parallelize
 from .transforms import assign_sparse_to_dense, sparsify_loops, gen_numba_code, intraloop_scalar_replacement
 from .transforms import remove_unused_array_stores, to_single_sparse_operand_form, to_inplace_sp_add_form
-from .transforms import convert_matmul_op_to_call
+from .transforms import check_for_undefined, convert_matmul_op_to_call
 
 def Index(*args):
     pass
@@ -49,7 +49,7 @@ def compile_from_src(src, **options):
         options["trie_fuse"] = True
         options["parallelize"] = True
     tree = ast.parse(src)
-    tree = apply_transform_on_ast(tree, "check_for_undefined", ["Tensor", "compile"])
+    tree = check_for_undefined.transform(tree)
     tree = apply_transform_on_ast(tree, "remove_func_decorator")
     tree = convert_matmul_op_to_call.transform(tree)
     tree = apply_transform_on_ast(tree, "to_single_op_form")
