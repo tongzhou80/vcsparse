fcba51bf9ac0df8ed1d9840837914502b87d3ff1
Tong Zhou <zt9465@gmail.com>
2024-11-24 14:47:35 +0800

    added a test C.multiply(A.T @ B)

--- tests/test_sparse_mul_matmul.py
+++ tests/test_sparse_mul_matmul.py
@@ -7,12 +7,17 @@ from react import *
 def f0(A: Tensor('i,k'), B: Tensor('k,j'), C: Tensor('i,j', 'csr')):
     return C.multiply(A @ B)
 
+@compile(dump_code=True, full_opt=True)
+def f1(A: Tensor('i,k'), B: Tensor('k,j'), C: Tensor('i,j', 'csr')):
+    return C.multiply(A.T @ B)
+
 def test():
     for N in [1000]:
         A = np.random.randn(N, N)
         B = np.random.randn(N, N)
         C = sp.random(N, N, density=0.01, format='csr')
         assert np.allclose(f0(A, B, C), C.multiply(A @ B).toarray())
+        assert np.allclose(f1(A, B, C), C.multiply(A.T @ B).toarray())
 
     
 if __name__ == '__main__':
