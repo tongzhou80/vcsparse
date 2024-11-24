ae7cf166b1e6f79fc9ce88ba9e04d68e1a156602
Tong Zhou <zt9465@gmail.com>
2024-11-24 14:01:27 +0800

    added test_transpose.py

---
+++ tests/test_transpose.py
@@ -0,0 +1,18 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+@compile(dump_code=True, full_opt=True)
+def f0(A: Tensor('i,k'), B: Tensor('k,j')):
+    return A.T @ B
+
+def test():
+    for N in [1000]:
+        A = np.random.randn(N, N)
+        B = np.random.randn(N, N)
+        assert np.allclose(f0(A, B), (A.T @ B))
+
+    
+if __name__ == '__main__':
+    test()
