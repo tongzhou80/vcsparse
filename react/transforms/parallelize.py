9b3c89b5240f38f85bf88298a083beb8f6762081
Tong Zhou <zt9465@gmail.com>
2024-11-24 23:17:50 +0800

    updated parallelize.py to visit_For

--- react/transforms/parallelize.py
+++ react/transforms/parallelize.py
@@ -2,16 +2,13 @@ import ast
 from ast_transforms.utils import *
 
 class Parallelize(ast.NodeTransformer):
-    def visit_FunctionDef(self, node):
-        for child in node.body:
-            if isinstance(child, ast.For):
-                if not hasattr(child, 'is_reduction'):
-                    old_range = child.iter
-                    child.iter = new_ast_call(
+    def visit_For(self, node):
+        if not hasattr(node, 'is_reduction'):
+            old_range = node.iter
+            node.iter = new_ast_call(
                         new_ast_attribute(new_ast_name('numba'), 'prange'),
                         old_range.args,
                     )
-
         return node
 
 def transform(node):
--- tests/test_sinkhorn.py
+++ tests/test_sinkhorn.py
@@ -4,8 +4,8 @@ import timeit
 from react import *
 from numpy import empty
 
-#@compile(dump_code=True, trie_fuse=True, memory_opt=True, gen_numba_code=True, parallelize=1)
-@compile(dump_code=True, full_opt=True)
+@compile(dump_code=True, trie_fuse=True, memory_opt=True, gen_numba_code=True, parallelize=1)
+#@compile(dump_code=True, trie_fuse=True, memory_opt=True, gen_numba_code=True)
 def f0(K: Tensor('k,i'), KT: Tensor('i,k'), x: Tensor('k,j'), c: Tensor('i,j', 'csr'), r: Tensor('k,i'), max_iter = 1):
     it = 0
     while it < max_iter:
@@ -25,20 +25,20 @@ def f0_py(K, KT, x, c, r, max_iter=1):
     return x
 
 def test():
-    NI = 100
-    NJ = 120
-    NK = 140
+    NI = 1000
+    NJ = 1200
+    NK = 1400
 
     K = np.random.randn(NK, NI)
     x = np.random.randn(NK, NJ)
-    c = sp.rand(NI, NJ, density=0.1, format='csr')
+    c = sp.rand(NI, NJ, density=0.01, format='csr')
     r = np.random.randn(NK, NI)
     #f0(K, K.T, x, c, r, max_iter=10)
     niters = 3
     x_0 = f0(K, K.T, x.copy(), c, r, max_iter=niters)
     x_1 = f0_py(K, K.T, x.copy(), c, r, max_iter=niters)
    
-    assert np.allclose(x_0, x_1)
+    assert np.allclose(x_0, x_1, atol=1e-5), (x_0[0], x_1[0])
 
     
 if __name__ == '__main__':
