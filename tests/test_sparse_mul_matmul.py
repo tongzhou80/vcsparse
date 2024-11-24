163cf0feb5d485dbaa98ca9dbd515caf3e618bf2
Tong Zhou <zt9465@gmail.com>
2024-11-24 14:20:32 +0800

    added test

---
+++ tests/test_sparse_mul_matmul.py
@@ -0,0 +1,19 @@
+import scipy.sparse as sp
+import numpy as np
+import timeit
+from react import *
+
+@compile(dump_code=True, full_opt=True)
+def f0(A: Tensor('i,k'), B: Tensor('k,j'), C: Tensor('i,j', 'csr')):
+    return C.multiply(A @ B)
+
+def test():
+    for N in [1000]:
+        A = np.random.randn(N, N)
+        B = np.random.randn(N, N)
+        C = sp.random(N, N, density=0.01, format='csr')
+        assert np.allclose(f0(A, B, C), C.multiply(A @ B).toarray())
+
+    
+if __name__ == '__main__':
+    test()
