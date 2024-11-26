d0ba006f834566cc9621ca21252fc08d10079c39
Tong Zhou <zt9465@gmail.com>
2024-11-26 20:56:34 +0800

    now make attach_index_notation attach a sparse_tensors attr to the functiondef node

--- react/transforms/attach_index_notation.py
+++ react/transforms/attach_index_notation.py
@@ -5,22 +5,19 @@ class AttachIndexNotation(ast.NodeTransformer):
     def __init__(self):
         self.indices_map = {}
         self.index_range = {}
-        self.tensor_format = {}
+        self.sparse_tensors = {}
 
     def visit_FunctionDef(self, node):
-        #dump(node.args)
         for arg in node.args.args:
             varname = arg.arg
             indices = []
             if hasattr(arg, 'annotation') and arg.annotation is not None:
                 index_str = arg.annotation.args[0].value
                 indices = index_str.split(',')
-                self.tensor_format[varname] = 'dense'
                 if len(arg.annotation.args) > 1:
                     format = arg.annotation.args[1].value
                     assert format in ('dense', 'csr'), "Only dense and csr format are supported for now!"
-                    self.tensor_format[varname] = format
-
+                    self.sparse_tensors[varname] = format
             self.indices_map[varname] = indices
             for pos,index in enumerate(indices):
                 if index not in self.index_range:
@@ -28,6 +25,7 @@ class AttachIndexNotation(ast.NodeTransformer):
                     self.index_range[index] = (varname, pos)
         node.indices_map = self.indices_map
         node.index_range = self.index_range
+        node.sparse_tensors = self.sparse_tensors
         self.generic_visit(node)
         return node
 
--- react/transforms/op_to_loop.py
+++ react/transforms/op_to_loop.py
@@ -21,11 +21,9 @@ class OpToLoop(ast.NodeTransformer):
     def __init__(self, trie_fuse=False):
         self.top_level_loops = []
         self.trie_fuse = trie_fuse
-        self.index_range = None
         self.indices_map = None
 
     def visit_FunctionDef(self, node):
-        self.index_range = node.index_range
         self.indices_map = node.indices_map
         self.generic_visit(node)
         return node
