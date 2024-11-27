3508ae2c04ac8c8a3506fa2ee34c88a7ad5ef6d8
Tong Zhou <zt9465@gmail.com>
2024-11-27 08:24:39 +0800

    now attach_iter_space_info contains iter space info for both sparse and dense indices

--- react/transforms/attach_iter_space_info.py
+++ react/transforms/attach_iter_space_info.py
@@ -13,33 +13,42 @@ class AttachSparseInfo(ast.NodeTransformer):
         return node
 
     def visit_Assign(self, node):
+        indices = []
+        iteration_spaces = {}
+        for v in node.use_vars + node.def_vars: 
+            for pos,idx in enumerate(self.indices_map[v]):
+                if idx not in indices:
+                    indices.append(idx)
+                    # Make the iteration space dense by default
+                    iteration_spaces[idx] = ('dense', v, pos)
+
         sparse_vars = {}
         for var in node.use_vars + node.def_vars:
             if var in self.sparse_tensors:
                 sparse_vars[var] = self.sparse_tensors[var]
 
-        index_range = {}
+        
         for var, format in sparse_vars.items():
             indices = self.indices_map[var]
             if format == 'csr':
                 # For CSR format, the second index is indexed by the first index
-                if indices[1] in index_range:
-                    assert index_range[indices[1]] == (var, indices[0]), "conflicting sparse ranges for index " + str(indices[1])
-                index_range[indices[1]] = (var, indices[0])
+                #if indices[1] in iteration_spaces:
+                #    assert iteration_spaces[indices[1]] == (var, indices[0]), "conflicting sparse ranges for index " + str(indices[1])
+                iteration_spaces[indices[1]] = ('sparse', var, indices[0])
             elif format == 'csc':
-                if indices[0] in index_range:
-                    assert index_range[indices[0]] == (var, indices[1]), "conflicting sparse ranges for index " + str(indices[0])
+                #if indices[0] in iteration_spaces:
+                #    assert iteration_spaces[indices[0]] == (var, indices[1]), "conflicting sparse ranges for index " + str(indices[0])
                 # For CSC format, the first index is indexed by the second index
-                index_range[indices[0]] = (var, indices[1])
+                iteration_spaces[indices[0]] = ('sparse', var, indices[1])
             else:
                 assert False, "Unsupported sparse format: " + format
 
         node.sparse_tensors = sparse_vars
-        node.sparse_index_ranges = index_range
-
-        dump_code(node)
-        print(node.sparse_tensors)
-        print(node.sparse_index_ranges)
+        node.sparse_iteration_spacess = iteration_spaces
+        node.indices = indices
+        # dump_code(node)
+        # print(node.sparse_tensors)
+        # print(node.sparse_iteration_spacess)
         self.generic_visit(node)
         return node
 
