fa73c09d3e87a0648611051a52d45747949dda90
Tong Zhou <zt9465@gmail.com>
2024-11-28 18:22:33 +0800

    updated spare * dense example to use sparse output

--- tests/test_sparse_mul_dense.py
+++ tests/test_sparse_mul_dense.py
@@ -3,11 +3,11 @@ import numpy as np
 import timeit
 from vcsparse import *
 
-@compile(dump_code=True, full_opt=True)
+@compile(dump_code=True, full_opt=True, use_sparse_output=True)
 def f0(A: Tensor('i,j', 'csr'), B: Tensor('i,j')):
     return A * B
 
-@compile(dump_code=True, full_opt=True)
+@compile(dump_code=True, full_opt=True, use_sparse_output=True)
 def f1(A: Tensor('i,j', 'csr'), alpha):
     return A * alpha
 
@@ -16,8 +16,8 @@ def test_sparse_mul_dense():
         A = sp.random(N, N, density=0.01, format='csr')
         B = np.random.randn(N, N)
         alpha = 0.1    
-        assert np.allclose(f0(A, B), (A.multiply(B)).toarray())
-        assert np.allclose(f1(A, alpha), (A.multiply(alpha)).toarray())
+        assert np.allclose(f0(A, B).toarray(), (A.multiply(B)).toarray())
+        assert np.allclose(f1(A, alpha).toarray(), (A.multiply(alpha)).toarray())
 
 
 if __name__ == '__main__':
