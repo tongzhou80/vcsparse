4c792154b745c20b91314affebc9d10546d1b5b0
Tong <tz@gatech.edu>
2024-12-21 01:22:54 -0500

    added create_inner_kernel

--- vcsparse/__init__.py
+++ vcsparse/__init__.py
@@ -9,7 +9,7 @@ from .transforms import assign_sparse_to_dense, sparsify_loops, gen_numba_code, 
 from .transforms import remove_unused_array_stores, to_single_sparse_operand_form, to_inplace_sp_add_form
 from .transforms import check_for_undefined, convert_matmul_op_to_call, remove_none_axis, mark_transpose_ops
 from .transforms import convert_sparse_multiply_call, fix_sparse_operand_to_left, attach_iter_space_info
-from .transforms import mark_sparse_output, rewrite_shape_attr_to_var
+from .transforms import mark_sparse_output, rewrite_shape_attr_to_var, create_inner_kernel
 
 def Index(*args):
     pass
@@ -92,6 +92,7 @@ def compile_from_src(src, **options):
     if options.get("gen_numba_code", False):
         if options.get("parallelize", False):
             tree = parallelize.transform(tree)
+        tree = create_inner_kernel.transform(tree)
         tree = gen_numba_code.transform(tree, options.get("parallelize", False))
 
     if options.get("memory_opt", False):
