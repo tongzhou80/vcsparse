f551fa895682acac10539f8a200b3f8ef1c75872
Tong <tz@gatech.edu>
2024-12-21 06:11:10 -0500

    briefly tried the manually vectorized version of dense_matmul_sparse and sparse_add_sparse

--- tests/gpu/test_dense_matmul_sparse.py
+++ tests/gpu/test_dense_matmul_sparse.py
@@ -7,6 +7,32 @@ from vcsparse import *
 def f0(A: Tensor('i,k'), B: Tensor('k,j', 'csr')):
     return A @ B
 
+import appy
+from cupy import empty, zeros, matmul, empty_like
+from cupyx.scipy.sparse import csr_matrix
+appy.config.tensorlib = 'cupy'
+@appy.jit
+def _f0(A, B_indptr, B_indices, B_data, B_shape):
+    __ret = empty((A.shape[0], B_shape[1]))
+    B_shape_1 = B_shape[1]
+    A_shape_0 = A.shape[0]
+    A_shape_1 = A.shape[1]
+    #pragma parallel for
+    for i in range(0, A_shape_0, 1):
+        #pragma simd
+        for j in range(0, B_shape_1, 1):
+            __ret[i, j] = 0
+
+        for k in range(0, A_shape_1, 1):
+            ##pragma simd
+            for __pB_k in range(B_indptr[k], B_indptr[k + 1], 1):
+                j = B_indices[__pB_k]
+                __ret[i, j] = __ret[i, j] + A[i, k] * B_data[__pB_k] # target_indices: ['i', 'j']
+    return __ret
+
+def f0(A, B):
+    return _f0(A, B.indptr, B.indices, B.data, B.shape)
+
 def test_dense_matmul_sparse():
     for N in [1000, 4000]:
         A = np.random.randn(N, N)
--- tests/gpu/test_sparse_add_sparse.py
+++ tests/gpu/test_sparse_add_sparse.py
@@ -13,8 +13,48 @@ def f1(A: Tensor('i,j', 'csr'), B: Tensor('i,j', 'csr'), C: Tensor('i,j', 'csr')
     # add three sparse
     return A + B + C
 
+import appy
+from cupy import empty, zeros, matmul, empty_like
+from cupyx.scipy.sparse import csr_matrix
+appy.config.tensorlib = 'cupy'
+@appy.jit
+def _f1(A_indptr, A_indices, A_data, A_shape, B_indptr, B_indices, B_data, B_shape, C_indptr, C_indices, C_data, C_shape):
+    __dB = empty((A_shape[0], A_shape[1]))
+    __v2 = empty((A_shape[0], A_shape[1]))
+    __ret = empty((A_shape[0], A_shape[1]))
+    __dB_shape_1 = __dB.shape[1]
+    __dB_shape_0 = __dB.shape[0]
+    __v2_shape_1 = __v2.shape[1]
+    __v2_shape_0 = __v2.shape[0]
+    __ret_shape_1 = __ret.shape[1]
+    __ret_shape_0 = __ret.shape[0]
+    #pragma parallel for
+    for i in range(0, __dB_shape_0, 1):
+        #pragma simd
+        for j in range(0, __dB_shape_1, 1):
+            __dB[i, j] = 0
+        for __pB_i in range(B_indptr[i], B_indptr[i + 1], 1):
+            j = B_indices[__pB_i]
+            __dB[i, j] = __dB[i, j] + B_data[__pB_i] * 1 # target_indices: ['i', 'j']
+        #pragma simd
+        for j in range(0, __v2_shape_1, 1):
+            __v2[i, j] = __dB[i, j]
+        for __pA_i in range(A_indptr[i], A_indptr[i + 1], 1):
+            j = A_indices[__pA_i]
+            __v2[i, j] = __v2[i, j] + A_data[__pA_i] # target_indices: ['i', 'j']
+        #pragma simd
+        for j in range(0, __ret_shape_1, 1):
+            __ret[i, j] = __v2[i, j]
+        for __pC_i in range(C_indptr[i], C_indptr[i + 1], 1):
+            j = C_indices[__pC_i]
+            __ret[i, j] = __ret[i, j] + C_data[__pC_i] # target_indices: ['i', 'j']
+    return __ret
+
+def f1(A, B, C):
+    return _f1(A.indptr, A.indices, A.data, A.shape, B.indptr, B.indices, B.data, B.shape, C.indptr, C.indices, C.data, C.shape)
+
 def test_sparse_add_sparse():
-    for N in [1000, 4000]:
+    for N in [1000, 2000, 4000, 8000]:
         A = sp.random(N, N, density=0.01, format='csr')
         B = sp.random(N, N, density=0.01, format='csr')
         C = sp.random(N, N, density=0.01, format='csr')
