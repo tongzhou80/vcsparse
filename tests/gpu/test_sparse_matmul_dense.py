0bfb9c3490bd5af1424188f148aa041938437e4e
Tong <tz@gatech.edu>
2024-12-29 05:23:33 -0500

    updated input setting

--- tests/gpu/test_sparse_matmul_dense.py
+++ tests/gpu/test_sparse_matmul_dense.py
@@ -10,17 +10,18 @@ def f0(A: Tensor('i,k', 'csr'), B: Tensor('k,j')):
     return A @ B
 
 def test_sparse_matmul_dense():
-    for N in [1000, 4000, 8000]:
-        A = sp.random(N, N, density=0.01, format='csr')
-        B = np.random.randn(N, N)
-        
-        #print(type(A.multiply(B)), type(A * alpha))
-        #print(np.allclose(A.multiply(B).toarray(), (A * B).toarray()))
-        assert np.allclose(f0(A, B), A @ B)
-        
-        t0 = triton.testing.do_bench(lambda: A @ B)
-        t1 = triton.testing.do_bench(lambda: f0(A, B))
-        print(t0, t1, f'{(t0/t1):.3f}')
+    for density in [0.001, 0.003, 0.01]:
+        for N in [1000, 4000, 8000]:
+            A = sp.random(N, N, density=density, format='csr')
+            B = np.random.randn(N, N)
+            
+            #print(type(A.multiply(B)), type(A * alpha))
+            #print(np.allclose(A.multiply(B).toarray(), (A * B).toarray()))
+            assert np.allclose(f0(A, B), A @ B)
+            
+            t0 = triton.testing.do_bench(lambda: A @ B)
+            t1 = triton.testing.do_bench(lambda: f0(A, B))
+            print(t0, t1, f'{(t0/t1):.3f}')
     
 
 if __name__ == '__main__':
